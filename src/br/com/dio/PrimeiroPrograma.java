94911c7472e4e681ee4aa227def861de66e963a0
RafaelDeVasconcelos <rafaelvasconcelos321@gmail.com>
2022-09-29 00:13:11 -0300

    Somente teste

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import br.com.dio.model.Gato;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        Gato gato = new Gato("Jhooy", "amarelo", 25);
+        System.out.println(gato);
+       /* System.out.println("Hello world!");*/
+
+    }
+}
